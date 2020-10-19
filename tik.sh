0cae034610d42a1927158de2654c76b4d26ba7bb
Rouze <50823809+rouze-d@users.noreply.github.com>
2020-10-19 13:30:52 +0800

    Update tik.sh

--- tik.sh
+++ tik.sh
@@ -24,16 +24,14 @@ if [ -z $GET ]; then #|| [ -z $POST ] ; then
     exit
 fi
 
-
-#down=`curl -s -k --url $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1`
-
-#down=`curl -s -k --url $GET --user-agent 'Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1`
-
-down=`curl -s -k --url $GET --user-agent 'Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video_mp4' | tail -n 1`
-
-#c=$(echo $1 | cut -d '@' -f 2 | cut -d '/' -f 1)
 c=$(echo $1 | cut -d '/' -f 4)
 b=$(echo $1 | cut -d '/' -f 6 | cut -d '?' -f 1)
 
+# two type URL request
+
+#down=`curl -s -k --url $GET --user-agent 'Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1`
+down=`curl -s -k --url https://m.tiktok.com/node/share/video/$c/$b --user-agent 'Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video_mp4' | tail -n 1`
+
+#c=$(echo $1 | cut -d '@' -f 2 | cut -d '/' -f 1)
+
 wget --referer=$GET --user-agent='Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --show-progress -c $down -O $c-$b.mp4
-#wget --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' --show-progress -c $down -O $c-$b.mp4
