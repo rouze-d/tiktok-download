53d57d1546e4d511b0657ebb006fc60c4f529cc8
Rouze <50823809+rouze-d@users.noreply.github.com>
2020-12-04 00:48:30 +0800

    Update tik.sh

--- tik.sh
+++ tik.sh
@@ -28,25 +28,26 @@ fi
 c=$(echo $1 | cut -d '/' -f 4)
 b=$(echo $1 | cut -d '/' -f 6 | cut -d '?' -f 1)
 
-echo -e ".tiktok.com	TRUE	/	TRUE	1636946512	tt_webid_v2	6895185694261839361
-.tiktok.com	TRUE	/	TRUE	1636946512	tt_webid	6895185694261839361
-.www.tiktok.com	TRUE	/	FALSE	1613928627	MONITOR_WEB_ID	8c80899a-7869-4397-8df6-acdbaf474f9b
-.tiktok.com	TRUE	/	TRUE	0	tt_csrf_token	RDaUIcQlHKb5P3nN594tHuOd
-www.tiktok.com	FALSE	/	FALSE	0	s_v_web_id	verify_khutsc6w_d9lZBWah_bAU1_4Zx5_9a1T_5FZuFc1gzRAx" > $pwd/.t_cookie.txt
+echo -e "
+.tiktok.com	TRUE	/	TRUE	1638549501	tt_webid_v2	6902070417072588293
+.tiktok.com	TRUE	/	TRUE	1638549501	tt_webid	6902070417072588293
+.www.tiktok.com	TRUE	/	FALSE	1614789735	MONITOR_WEB_ID	c21c1c96-5487-4bb7-ba44-826cae6e915a
+.tiktok.com	TRUE	/	TRUE	0	tt_csrf_token	z67BHbd7zejtEazcoeQ3meL6
+www.tiktok.com	FALSE	/	FALSE	0	s_v_web_id	verify_ki92hikg_6WOD9BbH_O8z0_4L4K_A43e_O8bbzca7bkzk
+.tiktok.com	TRUE	/	TRUE	1640709746	cookie-consent	{%22ga%22:true%2C%22af%22:true%2C%22fbp%22:true%2C%22lip%22:true%2C%22version%22:%22v2%22}
+" > $pwd/.t_cookie.txt
 
 #down=`curl -s -k --url $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1`
 
 #down=`curl --cookie .t_cookie.txt -s -k --url $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1`
 
 
-#down=`curl --cookie $pwd/.t_cookie.txt -s -k --url $GET --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep video_mp4 | head -n 1 | sed 's/amp;//g'`
+#down=`curl --cookie $pwd/.t_cookie.txt -s -k --url $GET --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep video_mp4 | head -n 1 | sed 's/amp;//g'`
 
-#down=`curl --cookie $pwd/.t_cookie.txt -s -k --url $GET --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep video_mp4 | head -n 1 | sed 's/\\u0026/\&/g'`
-
-down=`curl --cookie $pwd/.t_cookie.txt -s -k --url https://www.tiktok.com/node/share/video/$c/$b --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video_mp4' | tail -n 1`
+down=`curl -s -k --cookie $pwd/.t_cookie.txt --url https://www.tiktok.com/node/share/video/$c/$b --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video_mp4' | tail -n 1`
 
 #c=$(echo $1 | cut -d '@' -f 2 | cut -d '/' -f 1)
 
 
-wget --load-cookies=$pwd/".t_cookie.txt" --referer=$GET --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36' --show-progress -c $down -O $c-$b.mp4
+wget --load-cookies=$pwd/".t_cookie.txt" --referer=$GET --user-agent='Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --show-progress -c $down -O $c-$b.mp4
 #wget --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' --show-progress -c $down -O $c-$b.mp4
