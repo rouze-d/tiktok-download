7f105805127c8b312feac984ba140c543c87d326
Rouze <50823809+rouze-d@users.noreply.github.com>
2020-12-05 13:09:27 +0800

    Update tik.sh

--- tik.sh
+++ tik.sh
@@ -20,7 +20,7 @@ if [ -z $GET ]; then #|| [ -z $POST ] ; then
   ▌ ▄ ▌▗▘▌▞▀▖▌▗▘ ▌ ▌▞▀▖▌  ▌▛▀▖▐ ▞▀▖▝▀▖▞▀▌▞▀▖▙▀▖
   ▌ ▐ ▛▚ ▌▌ ▌▛▚  ▌ ▌▌ ▌▐▐▐ ▌ ▌▐ ▌ ▌▞▀▌▌ ▌▛▀ ▌  
   ▘ ▀▘▘ ▘▘▝▀ ▘ ▘ ▀▀ ▝▀  ▘▘ ▘ ▘ ▘▝▀ ▝▀▘▝▀▘▝▀▘▘$STAND by-$GREEN$BOLD rouze_d$STAND"
-    echo -e "$YELLOW$BOLD Missing video link URL:$STAND$BOLD bash $0 https://www.tiktok.com/@_fucktiktok/video/1234567890$STAND"
+    echo -e "$YELLOW$BOLD Missing video link URL:$STAND$BOLD bash $0 https://www.tiktok.com/@tiktokuser/video/1234567890$STAND"
     exit
 fi
 
@@ -49,5 +49,5 @@ down=`curl -s -k --cookie $pwd/.t_cookie.txt --url https://www.tiktok.com/node/s
 #c=$(echo $1 | cut -d '@' -f 2 | cut -d '/' -f 1)
 
 
-wget --load-cookies=$pwd/".t_cookie.txt" --referer=$GET --user-agent='Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --show-progress -c $down -O $c-$b.mp4
+wget --no-check-certificate --load-cookies=$pwd/".t_cookie.txt" --referer=$GET --user-agent='Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --show-progress -c $down -O $c-$b.mp4
 #wget --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' --show-progress -c $down -O $c-$b.mp4
