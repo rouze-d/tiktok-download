d0489e90bd1c4972da6eabb1b6204a03f1a35193
jaca <vitouejj@gmail.com>
2021-01-10 14:32:08 +0100

    declutter curl and wget arguments

--- tik.sh
+++ tik.sh
@@ -10,8 +10,9 @@ VIDEO_URL="$1"
     && echo "Missing video link URL, usage: ./tik.sh https://www.tiktok.com/@tiktok_user/video/1234567890" \
     && exit 1
 
-AUTHOR=$(echo $1 | cut -d '/' -f 4)
-ID=$(echo $1 | cut -d '/' -f 6 | cut -d '?' -f 1)
+AUTHOR=$(echo "$VIDEO_URL" | cut -d '/' -f 4)
+ID=$(echo "$VIDEO_URL" | cut -d '/' -f 6 | cut -d '?' -f 1)
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
 
 echo -e "
 .www.tiktok.com	TRUE	/	FALSE	1614874506	MONITOR_WEB_ID	b1c73e7a-e9b7-4f6c-8b95-1b49ab3227b8
@@ -22,17 +23,16 @@ echo -e "
 www.tiktok.com	FALSE	/	FALSE	0	s_v_web_id	verify_kiagygei_J4Frui45_RF6k_4r3t_AOr3_ju5FsZ6gWnNv
 " > "$PWD"/.t_cookie.txt
 
-down=$(curl --cookie "$PWD"/.t_cookie.txt \
-    -s \
-    -k \
-    --url "$VIDEO_URL" \
+DOWN=$(curl --cookie "$PWD"/.t_cookie.txt \
+    --silent \
     --referer "$VIDEO_URL" \
-    --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' \
-    --connect-timeout 90 | tr '"' '\n' | grep video_mp4 | head -n 1 | sed 's/amp;//g')
+    --user-agent "$USER_AGENT" \
+    --connect-timeout 90 "$VIDEO_URL" | tr '"' '\n' | grep video_mp4 | head -n 1 | sed 's/amp;//g')
 
 wget --load-cookies "$PWD"/.t_cookie.txt \
     --referer "$VIDEO_URL" \
-    --user-agent 'Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' \
-    --show-progress -c $down \
-    -O "$AUTHOR"-"$ID".mp4
+    --user-agent "$USER_AGENT" \
+    --show-progress \
+    --output-document "$AUTHOR"-"$ID".mp4 \
+    --continue "$DOWN"
 
