63baf47734e787905b8c5352f1b8ddd08d344141
jaca <vitouejj@gmail.com>
2021-01-10 15:44:03 +0100

    adhere to shellcheck recomendations in tok.sh

--- tok.sh
+++ tok.sh
@@ -1,28 +1,18 @@
-#!/bin/bash
-
-YELLOW=$(tput setaf 3)
-GREEN=$(tput setaf 2)
-RED=$(tput setaf 1)
-BLUE=$(tput setaf 4)
-GGG=$(tput setaf 5)
-CYN=$(tput setaf 7)
-STAND=$(tput sgr 0)
-BOLD=$(tput bold)
-
-
-echo -e "$RED$BOLD
- ▀▛▘▗ ▌ ▀▛▘  ▌   ▛▀▖          ▜         ▌      
-  ▌ ▄ ▌▗▘▌▞▀▖▌▗▘ ▌ ▌▞▀▖▌  ▌▛▀▖▐ ▞▀▖▝▀▖▞▀▌▞▀▖▙▀▖
-  ▌ ▐ ▛▚ ▌▌ ▌▛▚  ▌ ▌▌ ▌▐▐▐ ▌ ▌▐ ▌ ▌▞▀▌▌ ▌▛▀ ▌  
-  ▘ ▀▘▘ ▘▘▝▀ ▘ ▘ ▀▀ ▝▀  ▘▘ ▘ ▘ ▘▝▀ ▝▀▘▝▀▘▝▀▘▘$STAND by-$GREEN$BOLD rouze_d$STAND"
+#!/bin/sh
+#▀▛▘▗ ▌ ▀▛▘  ▌   ▛▀▖          ▜         ▌
+# ▌ ▄ ▌▗▘▌▞▀▖▌▗▘ ▌ ▌▞▀▖▌  ▌▛▀▖▐ ▞▀▖▝▀▖▞▀▌▞▀▖▙▀▖
+# ▌ ▐ ▛▚ ▌▌ ▌▛▚  ▌ ▌▌ ▌▐▐▐ ▌ ▌▐ ▌ ▌▞▀▌▌ ▌▛▀ ▌
+# ▘ ▀▘▘ ▘▘▝▀ ▘ ▘ ▀▀ ▝▀  ▘▘ ▘ ▘ ▘▝▀ ▝▀▘▝▀▘▝▀▘▘by rouze_d
 
 echo ""
 echo -e "Input Name : \c"
-read name
+read NAME
 
-mkdir $name
-cp tik.sh $name/
-cat *$name*.html | tr '"' '\n' | grep @ | grep '/video/' | grep '\.com/@' >> $name/$name.txt
-cd $name
-for x in `cat $name.txt`;do bash tik.sh $x;done
-echo "Done"
+mkdir "$NAME"
+cat *"$NAME"*.html | tr '"' '\n' | grep @ | grep '/video/' | grep '\.com/@' >> "$NAME"/"$NAME".txt
+
+while read x;
+do
+    ./tik.sh "$x" "$NAME"/
+done < "$NAME"/"$NAME".txt
+
