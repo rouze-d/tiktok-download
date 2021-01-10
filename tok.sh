97b0ac7dbd2f6ecb3e0ba7834d7e1990474f950e
jaca <vitouejj@gmail.com>
2021-01-10 15:47:52 +0100

    tok.sh no longer asks for name, it expects it as an argument

--- tok.sh
+++ tok.sh
@@ -4,14 +4,13 @@
 # ▌ ▐ ▛▚ ▌▌ ▌▛▚  ▌ ▌▌ ▌▐▐▐ ▌ ▌▐ ▌ ▌▞▀▌▌ ▌▛▀ ▌
 # ▘ ▀▘▘ ▘▘▝▀ ▘ ▘ ▀▀ ▝▀  ▘▘ ▘ ▘ ▘▝▀ ▝▀▘▝▀▘▝▀▘▘by rouze_d
 
-echo ""
-echo -e "Input Name : \c"
-read NAME
+NAME="$1"
+[ -z "$NAME" ] && echo 'Please specify a TikTokers username' && exit 1
 
 mkdir "$NAME"
 cat *"$NAME"*.html | tr '"' '\n' | grep @ | grep '/video/' | grep '\.com/@' >> "$NAME"/"$NAME".txt
 
-while read x;
+while read -r x;
 do
     ./tik.sh "$x" "$NAME"/
 done < "$NAME"/"$NAME".txt
