43b0e35efe13805a7d578fab4af4f8903677433d
jaca <vitouejj@gmail.com>
2021-01-10 14:05:18 +0100

    refactor error reporting

--- tik.sh
+++ tik.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
-
-YELLOW=$(tput setaf 3)
-GREEN=$(tput setaf 2)
-RED=$(tput setaf 1)
-BLUE=$(tput setaf 4)
-GGG=$(tput setaf 5)
-CYN=$(tput setaf 7)
-STAND=$(tput sgr 0)
-BOLD=$(tput bold)
+# ▀▛▘▗ ▌ ▀▛▘  ▌   ▛▀▖          ▜         ▌
+#  ▌ ▄ ▌▗▘▌▞▀▖▌▗▘ ▌ ▌▞▀▖▌  ▌▛▀▖▐ ▞▀▖▝▀▖▞▀▌▞▀▖▙▀▖
+#  ▌ ▐ ▛▚ ▌▌ ▌▛▚  ▌ ▌▌ ▌▐▐▐ ▌ ▌▐ ▌ ▌▞▀▌▌ ▌▛▀ ▌
+#  ▘ ▀▘▘ ▘▘▝▀ ▘ ▘ ▀▀ ▝▀  ▘▘ ▘ ▘ ▘▝▀ ▝▀▘▝▀▘▝▀▘▘ by rouze_d
 
 
 GET="$1"
+[ -z "$GET" ] \
+    && echo "Missing video link URL, usage: ./tik.sh https://www.tiktok.com/@tiktok_user/video/1234567890" \
+    && exit 1
+
 pwd=$(pwd)
-
-
-if [ -z $GET ]; then #|| [ -z $POST ] ; then
-    echo -e "$RED$BOLD
- ▀▛▘▗ ▌ ▀▛▘  ▌   ▛▀▖          ▜         ▌      
-  ▌ ▄ ▌▗▘▌▞▀▖▌▗▘ ▌ ▌▞▀▖▌  ▌▛▀▖▐ ▞▀▖▝▀▖▞▀▌▞▀▖▙▀▖
-  ▌ ▐ ▛▚ ▌▌ ▌▛▚  ▌ ▌▌ ▌▐▐▐ ▌ ▌▐ ▌ ▌▞▀▌▌ ▌▛▀ ▌  
-  ▘ ▀▘▘ ▘▘▝▀ ▘ ▘ ▀▀ ▝▀  ▘▘ ▘ ▘ ▘▝▀ ▝▀▘▝▀▘▝▀▘▘$STAND by-$GREEN$BOLD rouze_d$STAND"
-    echo -e "$YELLOW$BOLD Missing video link URL:$STAND$BOLD bash $0 https://www.tiktok.com/@tiktok_user/video/1234567890$STAND"
-    exit
-fi
-
 
 c=$(echo $1 | cut -d '/' -f 4)
 b=$(echo $1 | cut -d '/' -f 6 | cut -d '?' -f 1)
