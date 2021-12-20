97faa5f77b294888d98c30e19de818819fbb0688
Rouze <50823809+rouze-d@users.noreply.github.com>
2021-12-20 22:19:25 +0800

    Update tik.sh

--- tik.sh
+++ tik.sh
@@ -1,31 +1,57 @@
-#!/bin/sh
-# ▀▛▘▗ ▌ ▀▛▘  ▌   ▛▀▖          ▜         ▌
-#  ▌ ▄ ▌▗▘▌▞▀▖▌▗▘ ▌ ▌▞▀▖▌  ▌▛▀▖▐ ▞▀▖▝▀▖▞▀▌▞▀▖▙▀▖
-#  ▌ ▐ ▛▚ ▌▌ ▌▛▚  ▌ ▌▌ ▌▐▐▐ ▌ ▌▐ ▌ ▌▞▀▌▌ ▌▛▀ ▌
-#  ▘ ▀▘▘ ▘▘▝▀ ▘ ▘ ▀▀ ▝▀  ▘▘ ▘ ▘ ▘▝▀ ▝▀▘▝▀▘▝▀▘▘ by rouze_d
+#!/bin/bash
+
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+BLUE=$(tput setaf 4)
+GGG=$(tput setaf 5)
+CYN=$(tput setaf 7)
+STAND=$(tput sgr 0)
+BOLD=$(tput bold)
 
 
-TIKTOK_URL="$1"
-[ -z "$TIKTOK_URL" ] \
-    && echo "Missing video link URL, usage: ./tik.sh https://www.tiktok.com/@tiktok_user/video/1234567890 [destination_folder]" \
-    && exit 1
-DESTINATION_FOLDER=$2
-
-AUTHOR=$(echo "$TIKTOK_URL" | cut -d '/' -f 4)
-ID=$(echo "$TIKTOK_URL" | cut -d '/' -f 6 | cut -d '?' -f 1)
-USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-
-VIDEO_URL=$(curl --cookie "$PWD"/.cookie.txt \
-    --silent \
-    --referer "$TIKTOK_URL" \
-    --user-agent "$USER_AGENT" \
-    --connect-timeout 90 "$TIKTOK_URL" | tr '"' '\n' | grep video_mp4 | head -n 1 | sed 's/amp;//g')
-
-curl --progress-bar \
-    --cookie "$PWD"/.cookie.txt \
-    --referer "$TIKTOK_URL" \
-    --user-agent "$USER_AGENT" \
-    --output "$DESTINATION_FOLDER""$AUTHOR"-"$ID".mp4 \
-    --continue-at - "$VIDEO_URL"
+GET="$1"
+pwd=$(pwd)
 
 
+if [ -z $GET ]; then #|| [ -z $POST ] ; then
+    echo -e "$RED$BOLD
+ ▀▛▘▗ ▌ ▀▛▘  ▌   ▛▀▖          ▜         ▌      
+  ▌ ▄ ▌▗▘▌▞▀▖▌▗▘ ▌ ▌▞▀▖▌  ▌▛▀▖▐ ▞▀▖▝▀▖▞▀▌▞▀▖▙▀▖
+  ▌ ▐ ▛▚ ▌▌ ▌▛▚  ▌ ▌▌ ▌▐▐▐ ▌ ▌▐ ▌ ▌▞▀▌▌ ▌▛▀ ▌  
+  ▘ ▀▘▘ ▘▘▝▀ ▘ ▘ ▀▀ ▝▀  ▘▘ ▘ ▘ ▘▝▀ ▝▀▘▝▀▘▝▀▘▘$STAND by-$GREEN$BOLD rouze_d$STAND"
+    echo -e "$YELLOW$BOLD Missing video link URL:$STAND$BOLD bash $0 https://www.tiktok.com/@tiktokuser/video/1234567890$STAND"
+    exit
+fi
+
+
+c=$(echo $1 | cut -d '/' -f 4)
+b=$(echo $1 | cut -d '/' -f 6 | cut -d '?' -f 1)
+
+echo -e "
+.www.tiktok.com	TRUE	/	FALSE	1614874506	MONITOR_WEB_ID	b1c73e7a-e9b7-4f6c-8b95-1b49ab3227b8
+.tiktok.com	TRUE	/	FALSE	1608332062	CONSENT	YES+
+.tiktok.com	TRUE	/	TRUE	1638633279	tt_webid	6902430212190832130
+.tiktok.com	TRUE	/	TRUE	1638633279	tt_webid_v2	6902430212190832130
+.tiktok.com	TRUE	/	TRUE	0	tt_csrf_token	Htv_7mtkaH1D_ehZr3f84Y5Y
+www.tiktok.com	FALSE	/	FALSE	0	s_v_web_id	verify_kiagygei_J4Frui45_RF6k_4r3t_AOr3_ju5FsZ6gWnNv
+" > .t_cookie.txt
+
+# option 1 for download method
+down=`curl --cookie $pwd/.t_cookie.txt -s -k --url https://www.tiktok.com/node/share/video/$c/$b --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video_mp4' | tail -n 1`
+
+# option 2 for download method
+#down=`curl --cookie .t_cookie.txt -s -k --url $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1`
+
+# option 3 for download method
+#down=`curl --cookie $pwd/.t_cookie.txt -s -k --url $GET --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep video_mp4 | head -n 1 | sed 's/amp;//g'`
+
+# option 4 for download method
+#down=`curl --cookie .t_cookie.txt -s -k  --url $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1 | sed 's/\\u0026/\&/g'`
+
+
+#c=$(echo $1 | cut -d '@' -f 2 | cut -d '/' -f 1)
+
+wget --load-cookies .t_cookie.txt --referer=$GET --user-agent 'Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --show-progress -c $down -O $c-$b.mp4
+
+#wget --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' --show-progress -c $down -O $c-$b.mp4
