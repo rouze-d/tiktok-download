74b36f5565904bf7cd739b3ed0f54aa253bfdc94
Rouze <50823809+rouze-d@users.noreply.github.com>
2022-10-15 08:48:15 +0800

    Update tik.sh

--- tik.sh
+++ tik.sh
@@ -28,18 +28,18 @@ fi
 c=$(echo $1 | cut -d '/' -f 4)
 b=$(echo $1 | cut -d '/' -f 6 | cut -d '?' -f 1)
 
-echo -e "
-.www.tiktok.com	TRUE	/	FALSE	1614874506	MONITOR_WEB_ID	b1c73e7a-e9b7-4f6c-8b95-1b49ab3227b8
-.tiktok.com	TRUE	/	FALSE	1608332062	CONSENT	YES+
-.tiktok.com	TRUE	/	TRUE	1638633279	tt_webid	6902430212190832130
-.tiktok.com	TRUE	/	TRUE	1638633279	tt_webid_v2	6902430212190832130
-.tiktok.com	TRUE	/	TRUE	0	tt_csrf_token	Htv_7mtkaH1D_ehZr3f84Y5Y
-www.tiktok.com	FALSE	/	FALSE	0	s_v_web_id	verify_kiagygei_J4Frui45_RF6k_4r3t_AOr3_ju5FsZ6gWnNv
-" >> .t_cookie.txt
+# echo -e "
+# .www.tiktok.com	TRUE	/	FALSE	1614874506	MONITOR_WEB_ID	b1c73e7a-e9b7-4f6c-8b95-1b49ab3227b8
+# .tiktok.com	TRUE	/	FALSE	1608332062	CONSENT	YES+
+# .tiktok.com	TRUE	/	TRUE	1638633279	tt_webid	6902430212190832130
+# .tiktok.com	TRUE	/	TRUE	1638633279	tt_webid_v2	6902430212190832130
+# .tiktok.com	TRUE	/	TRUE	0	tt_csrf_token	Htv_7mtkaH1D_ehZr3f84Y5Y
+#www.tiktok.com	FALSE	/	FALSE	0	s_v_web_id	verify_kiagygei_J4Frui45_RF6k_4r3t_AOr3_ju5FsZ6gWnNv
+#" >> .t_cookie.txt
 
 # option 1 for download method
-title=`curl --cookie $pwd/.t_cookie.txt -s -k --url https://www.tiktok.com/node/share/video/$c/$b --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | grep '#' | tr '{' '\n' | head | grep 'title\"\:' | cut -d '"' -f4`
-down=`curl --cookie $pwd/.t_cookie.txt -s -k --url https://www.tiktok.com/node/share/video/$c/$b --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video_mp4' | tail -n 1`
+#title=`curl --cookie $pwd/.t_cookie.txt -s -k --url https://www.tiktok.com/node/share/video/$c/$b --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | grep '#' | tr '{' '\n' | head | grep 'title\"\:' | cut -d '"' -f4`
+#down=`curl --cookie $pwd/.t_cookie.txt -s -k --url https://www.tiktok.com/node/share/video/$c/$b --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video_mp4' | tail -n 1`
 
 # option 2 for download method
 #down=`curl --cookie .t_cookie.txt -s -k --url $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1`
@@ -50,10 +50,19 @@ down=`curl --cookie $pwd/.t_cookie.txt -s -k --url https://www.tiktok.com/node/s
 # option 4 for download method
 #down=`curl --cookie .t_cookie.txt -s -k  --url $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' | tr '"' '\n' | grep -i -E 'video/[a-z]'\|'.mp4' | tail -n 1 | sed 's/\\u0026/\&/g'`
 
+# option 5 for download method
+title=$(curl -s -k --url $GET --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '>' '\n' | grep '\/title' | cut -d '<' -f1)
+down=$(curl -s -k --url $GET --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 | tr '"' '\n' | grep video_mp4 | head -n 1 |  sed 's/\\u002F/\//g' | sed 's/\%7C/|/g' | sed 's/\%3D/=/g')
+
+
 
 #c=$(echo $1 | cut -d '@' -f 2 | cut -d '/' -f 1)
 echo -e "\n${YELLOW}video title/caption :${BOLD}${GREEN} ${title} ${STAND}"
-wget --load-cookies $pwd/.t_cookie.txt --quiet --referer=$GET --user-agent 'Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10' --show-progress -c $down -O $c-$b.mp4
-echo -e "\nvideo : ${c}-${b}.mp4\ntitle/caption : ${title}" >> ${c}-title.txt
+
+#curl -s -k --url ${down} --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' --connect-timeout 90 -o $c-$b.mp4
+
+wget --quiet --referer $GET --user-agent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'  --show-progress -c $down -O $c-$b.mp4
+
+#echo -e "\nvideo : ${c}-${b}.mp4\ntitle/caption : ${title}" >> ${c}-title.txt
 
 #wget --user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36' --show-progress -c $down -O $c-$b.mp4
